b5a327777b4a5edd60a19ac29a7d806552a5b5a6
Yuren Zhang <yz497@cam.ac.uk>
2018-08-28 22:21:20 +0000

    Create compare page without comparing functionality

--- routes/compare.js
+++ routes/compare.js
@@ -3,7 +3,13 @@ var router  = express.Router();
 
 // root route
 router.get("/", function(req, res) {
-    res.render("landing");
+    res.render("compare");
+});
+
+// compare route
+router.post("/", function(req, res) {
+    console.log(req.body);
+    res.render("compare");
 });
 
 // module exports
